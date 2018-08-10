e6faaa11f564215ce1cb038aecdb3effe5f0c982
Srijan Duggal <srijanduggal17@gmail.com>
2018-08-10 10:13:37 -0500

    Documented code

--- Development/js/gmat.js
+++ Development/js/gmat.js
@@ -1,11 +1,12 @@
+//Firebase initialization
 const config = {
 	apiKey: "AIzaSyBmNSU0Ukje3nEFARhxIL_ba8jwlgvwspQ",
 	databaseURL: "https://powercourses-eea9f.firebaseio.com"
 };
 firebase.initializeApp(config);
 
+//Gets data from firebase and updates sidebar photo
 const webRef = firebase.database().ref('website');
-
 webRef.once('value')
 	.then(snapshot => {
 		const webData = snapshot.val();
